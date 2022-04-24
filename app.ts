75f7b47f45b9e7014d18f18f945f9cbdca500585
Bhatti <2093317@cognizant.com>
2022-04-24 16:30:27 +0530

    enum type in ts

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
 // console.log('Your code goes here...');
 
-const persons: {
-name: string,
-age: number,
-hobbies: string [],
-// Below role is an example of tuple type in typescript
-role: [number, string]
-} = {
+enum Role {ADMIN, READ_ONLY, AUTHOR, USER}
+
+const persons= {
     name: "Abhishek",
     age: 32,
     hobbies: ['sports', 'cooking'],
-    role:[2,"team lead"]
+    role: Role.AUTHOR
+}
+
+if (persons.role=== Role.AUTHOR) {
+    console.log("Author")
 }
