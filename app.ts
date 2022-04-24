f41f8b6ff4b100d9d5a2390687da9f7d88240e95
Bhatti <2093317@cognizant.com>
2022-04-24 15:43:57 +0530

    tuple type in ts

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+// console.log('Your code goes here...');
+
+const persons: {
+name: string,
+age: number,
+hobbies: string [],
+// Below role is an example of tuple type in typescript
+role: [number, string]
+} = {
+    name: "Abhishek",
+    age: 32,
+    hobbies: ['sports', 'cooking'],
+    role:[2,"team lead"]
+}
