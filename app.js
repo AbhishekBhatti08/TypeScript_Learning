dcf00ff40eecc733a65f3c451f337b782f41ee1d
Bhatti <2093317@cognizant.com>
2022-05-02 12:28:59 +0530

    class and inheritance in ts

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 "use strict";
-function Add(n1, n2) {
-    return n1 + n2;
+// function Add ( n1:number, n2: number) {
+//  return n1 + n2;
+// }
+// function printResult (num: number): void {
+//     console.log("Result -->" + num)
+// }
+// printResult(Add(2,4));
+// let combineNums: (n1:number, n2:number)=>  number = Add;
+// combineNums(3,3);
+class Employee {
+    constructor() {
+        this.id = [];
+        //  private employees: string[]= [];
+        this.name = [];
+        this.name = ["Anna", "Michael"],
+            this.id = [1, 2];
+    }
+    printData() {
+        console.log(this.name);
+    }
 }
-function printResult(num) {
-    console.log("Result -->" + num);
+let abc = new Employee;
+;
+abc.printData();
+class Data extends Employee {
 }
-printResult(Add(2, 4));
-let combineNums = Add;
-combineNums(3, 3);
--- app.ts
+++ app.ts
@@ -1,15 +1,52 @@
 
 
-function Add ( n1:number, n2: number) {
- return n1 + n2;
+// function Add ( n1:number, n2: number) {
+//  return n1 + n2;
+// }
+
+// function printResult (num: number): void {
+//     console.log("Result -->" + num)
+// }
+
+// printResult(Add(2,4));
+
+// let combineNums: (n1:number, n2:number)=>  number = Add;
+
+// combineNums(3,3);
+
+
+class Employee {
+
+ private id: number [] = [];
+//  private employees: string[]= [];
+ private name:string[]=[];
+
+
+ constructor() {
+     this.name = ["Anna","Michael"],
+     this.id=[1,2]
+ }
+
+ printData(){
+     console.log(this.name)
+ }
+
+
 }
 
-function printResult (num: number): void {
-    console.log("Result -->" + num)
-}
+let abc = new Employee;;
+abc.printData();
 
-printResult(Add(2,4));
+class Data extends Employee {
+    constructor(id: number, private reports: string[]){
+        super();
+    }
 
-let combineNums: (n1:number, n2:number)=>  number = Add;
+    addReport(text: string){
+        this.reports.push(text);
+    }
 
-combineNums(3,3);
+    printReport(){
+        console.log(this.reports)
+    }
+}   
